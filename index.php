d36ae22acccdaa5cfb5344cb3695e983884bfb80
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 04:13:36 -0800

    Blob test 9

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ use MicrosoftAzure\Storage\Common\ServiceException;
 
 echo "Hello!";
 
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=rakeshphotos;AccountKey=TsV+ILARvx/vtkRg9eM7j6REB517SAu9ne8jzvuTtILRUSV0fEKKqbwwE1iPqkLR73xt3vgoTCzgHXyeeVTxDQ==";
+$connectionString = "DefaultEndpointsProtocol='https';AccountName='rakeshphotos';AccountKey='TsV+ILARvx/vtkRg9eM7j6REB517SAu9ne8jzvuTtILRUSV0fEKKqbwwE1iPqkLR73xt3vgoTCzgHXyeeVTxDQ=='";
 
 // Create blob REST proxy.
 $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
