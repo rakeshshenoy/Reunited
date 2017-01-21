329ce0343be9826fb864d8138e76de3272565fe1
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 01:33:46 -0800

    Query test

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 1);
 try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
