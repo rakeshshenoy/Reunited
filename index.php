7ee728ce2947e27cc22402f3d813e4ee385c1e32
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 00:50:55 -0800

    Create table test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE TABLE test (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id INT(6) AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(30) NOT NULL
     )";
     $conn->exec($sql);
