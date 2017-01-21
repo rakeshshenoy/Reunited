7ad4b5ec5403825d2fe6edfd78b0c00d7f1207be
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 01:43:40 -0800

    Query test 2

--- index.php
+++ index.php
@@ -3,10 +3,12 @@ ini_set('display_errors', 1);
 try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = $conn->prepare('SELECT id FROM test');
+    $query = $conn->prepare("INSERT INTO test  (id,name) VALUES ('2','Snehal')");
+    $query->execute();
+    $query = $conn->prepare('SELECT * FROM test');
     $query->execute();
 	while ($row = $query->fetch()) {
-        echo $row['id'];
+        echo $row['name'];
 	}
 }
 catch (PDOException $e) {
