b21656a9c8afcd048f0b49da6d259548d55e82a7
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 01:35:43 -0800

    Query test

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = $conn->mssql_query('SELECT id FROM test');
-
+    $query = $conn->prepare('SELECT id FROM test');
+    $query->execute();
 	// Check if there were any records
 	/*if (!mssql_num_rows($query)) {
 	    echo 'No records found';
@@ -13,9 +13,6 @@ try {
         	echo $row['id'], $row['name'], PHP_EOL;
 	    }
 	}*/
-
-	// Free the query result
-	mssql_free_result($query);
 }
 catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
