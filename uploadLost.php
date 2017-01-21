05376445b96f82d9cf95f37ad9aa34c5cb5b645a
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 14:58:55 -0800

    Face API Matt Damon10

--- uploadLost.php
+++ uploadLost.php
@@ -85,7 +85,8 @@
 		}
 
 		$jsonstring = $response->getBody();
-		echo json_decode($jsonstring)[0]["faceId"];
+		$faceID = json_decode($jsonstring)[0]["faceId"];
+		echo $faceID;
 		//echo $name.' '.$contactName.' '.$contactPhone;
 		//echo $response;
 		//echo $faceID.PHP_EOL;
