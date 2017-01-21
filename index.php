a9da4047425f146491f0ad416e27c589e88f75b5
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 00:33:10 -0800

    Fifth

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Success!";
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
