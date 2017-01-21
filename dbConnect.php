aa34fc4b56e2c47f1caf6d8d568e3facdb81fe93
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 03:29:17 -0800

    Test dbConnect

---
+++ dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+	try {
+    	$conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	echo "Success!";
+    }
+    catch (PDOException $e) {
+    	echo $sql . "<br>" . $e->getMessage();
+	}
+?>
--- index.php
+++ index.php
@@ -1,15 +1,45 @@
 <?php
-ini_set('display_errors', 1);
+require_once 'vendor\autoload.php';
+require_once 'dbConnect.php';
+
+use WindowsAzure\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use MicrosoftAzure\Storage\Common\ServiceException;
+
+// Create blob REST proxy.
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
+
+$content = fopen("c:\myfile.txt", "r");
+$blob_name = "myblob";
+
+try    {
+    //Upload blob
+    $blobRestProxy->createBlockBlob("photos", $blob_name, $content);
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/azure/dd179439.aspx
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
+
 try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = $conn->prepare("INSERT INTO test  (id,name) VALUES ('2','Snehal')");
+
+
+
+    /*$query = $conn->prepare("INSERT INTO test  (id,name) VALUES ('2','Snehal')");
     $query->execute();
     $query = $conn->prepare('SELECT * FROM test');
     $query->execute();
 	while ($row = $query->fetch()) {
         echo $row['name'];
-	}
+	}*/
 }
 catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
