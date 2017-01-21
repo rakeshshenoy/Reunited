76b7216cf2f5787b51dbd8af2b5514b3284bc8bb
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 00:31:43 -0800

    Fourth

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $server = "tcp:rakesh-sql.database.windows.net,1433";
-    $username = "rakeshshenoy@rakesh-sql";
-    $password = "Rakesh123";
-    $dbname = "rakesh-db";
-    $conn = new PDO("mysql:host=$server;dbname=$dbname",$username,$password);
-    echo "Success!\n";
+    $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch ( PDOException $e ) {
-   print( "Error connecting to SQL Server." );
-   die(print_r($e));
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 ?>
