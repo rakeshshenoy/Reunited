061577acc8edb0493a4c19e7c967dbb8f44e4977
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 01:09:15 -0800

    Select table test

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = "INSERT INTO test  (id,name) VALUES   
-('1','Rakesh')";
-    $conn->exec($query);
-    echo "Table created!";
+    $query = "Select * from test";
+    $result = $conn->exec($query);
+    echo $result;
+    echo "\nSuccess!";
 }
 catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
