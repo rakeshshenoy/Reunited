8fef3a924e12362575d3d3aa3272eae98e08f862
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 01:16:33 -0800

    Select table test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:rakesh-sql.database.windows.net,1433; Database = rakesh-db", "rakeshshenoy", "Rakesh123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = "Select * from test";
+    $query = "SELECT * FROM test";
     $result = $conn->exec($query);
     echo $result;
     echo "\nSuccess!";
