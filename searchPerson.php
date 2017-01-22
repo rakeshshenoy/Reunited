0339ed9b881a89c28f55823a416d315b69afc059
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 20:28:22 -0800

    Search Person Hello

--- searchPerson.php
+++ searchPerson.php
@@ -1,12 +1,12 @@
 <?php
-	require_once 'HTTP/Request2.php';
+	/*require_once 'HTTP/Request2.php';
 	require_once 'vendor/autoload.php';
 	require_once 'dbConnect.php';
 
 	use WindowsAzure\Common\ServicesBuilder;
 	use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 	use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
-	use MicrosoftAzure\Storage\Common\ServiceException;
+	use MicrosoftAzure\Storage\Common\ServiceException;*/
 
 	echo "Hello!";
 
