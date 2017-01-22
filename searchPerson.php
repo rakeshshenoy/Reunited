1806fe25233bdd9550c32db45ce2cbd4c22a4521
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-22 03:47:51 -0800

    Perfect Face ID

--- searchPerson.php
+++ searchPerson.php
@@ -2,12 +2,12 @@
 
 echo "Hello!"
 // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
-/*require_once 'HTTP/Request2.php';
+require_once 'HTTP/Request2.php';
 
 $request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/verify');
 $url = $request->getUrl();
-
-$headers = array(
+echo "1";
+/*$headers = array(
     // Request headers
     'Content-Type' => 'application/json',
     'Ocp-Apim-Subscription-Key' => 'dd51642516ac431a9c593b4c78b8a806'
