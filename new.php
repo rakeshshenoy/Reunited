24cebf0c396fb59e5d1d065f045e9cb4c2f234cd
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 23:40:28 -0800

    Perfect Face ID

--- new.php
+++ new.php
@@ -9,52 +9,53 @@
 		$connectionString = "DefaultEndpointsProtocol='https';AccountName='rakeshphotos';AccountKey='f2HkSZYQm65pkJ9L4ungJqD7FIMU+5oGIdpxug1eL4YQxz7IuS4ZxIA4Q56BfpNBHL4Zo5cBY0kHFQs7yjUtwg=='";
 		// Create blob REST proxy.
 		$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+		if($_SERVER['REQUEST_METHOD']=='POST'){
+			try    {
+				$image = $_POST['image'];
+				$content = base64_decode($image);
+				$blobRestProxy->createBlockBlob("photos", 0, $content);
+			    $mainblob = $blobRestProxy->getBlob("photos", 0);
+			    $mainimg = $mainblob->getContentStream();
+			    $blobRestProxy->deleteBlob("photos", 0);
 
-		try    {
-			/*$image = $_POST['image'];
-			$content = base64_decode($image);
-			$blobRestProxy->createBlockBlob("photos", 0, $content);
-		    $mainblob = $blobRestProxy->getBlob("photos", 0);
-		    $mainimg = $mainblob->getContentStream();
-		    $blobRestProxy->deleteBlob("photos", 0);*/
-
-			// Get blob.
-			$blob = $blobRestProxy->getBlob("photos", $id);
-			$x = $blob->getContentStream();
-			//echo $x;
-			$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/detect');
-			$url = $request->getUrl();
-			$headers = array(
-			    // Request headers
-			    'Content-Type' => 'application/octet-stream',
-			    'Ocp-Apim-Subscription-Key' => 'dd51642516ac431a9c593b4c78b8a806',
-			);
-			$request->setHeader($headers);
-			$parameters = array(
-			    // Request parameters
-			    'returnFaceId' => 'true'
-			);
-			$url->setQueryVariables($parameters);
-			$request->setMethod(HTTP_Request2::METHOD_POST);
-			$request->setBody($x);
-			try
-			{
-			    $response = $request->send();
+				// Get blob.
+				//$blob = $blobRestProxy->getBlob("photos", $id);
+				//$x = $blob->getContentStream();
+				//echo $x;
+				$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/detect');
+				$url = $request->getUrl();
+				$headers = array(
+				    // Request headers
+				    'Content-Type' => 'application/octet-stream',
+				    'Ocp-Apim-Subscription-Key' => 'dd51642516ac431a9c593b4c78b8a806',
+				);
+				$request->setHeader($headers);
+				$parameters = array(
+				    // Request parameters
+				    'returnFaceId' => 'true'
+				);
+				$url->setQueryVariables($parameters);
+				$request->setMethod(HTTP_Request2::METHOD_POST);
+				$request->setBody($mainimg);
+				try
+				{
+				    $response = $request->send();
+				}
+				catch (HttpException $ex)
+				{
+				    echo $ex;
+				}
+				$y = $response->getBody();
+				$z = json_decode($y)[0]->{"faceId"};
+				echo $z;
+				//echo $response;
 			}
-			catch (HttpException $ex)
-			{
-			    echo $ex;
+			catch(ServiceException $e){
+				$code = $e->getCode();
+				$error_message = $e->getMessage();
+				echo $code.": ".$error_message."<br />";
+				//return NULL;
 			}
-			$y = $response->getBody();
-			$z = json_decode($y)[0]->{"faceId"};
-			echo $z;
-			//echo $response;
-		}
-		catch(ServiceException $e){
-			$code = $e->getCode();
-			$error_message = $e->getMessage();
-			echo $code.": ".$error_message."<br />";
-			//return NULL;
 		}
 		//return NULL;
 	//}
