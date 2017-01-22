246e4985c702c784ceece0583790e4babd8b88dd
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 21:15:58 -0800

    Search Person Hello

--- searchPerson.php
+++ searchPerson.php
@@ -12,7 +12,7 @@
 	{
 		// Get Face ID using Microsoft
 		echo "hi".PHP_EOL;
-		$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/detect');
+		/*$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/detect');
 		$url = $request->getUrl();
 
 		$headers = array(
@@ -30,7 +30,7 @@
 
 		$url->setQueryVariables($parameters);
 
-		$request->setMethod(HTTP_Request2::METHOD_POST);
+		$request->setMethod(HTTP_Request2::METHOD_POST);*/
 
 		// Request body
 		//$request->setBody("{'url':".$imgurl."}");
