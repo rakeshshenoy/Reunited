1acdc4ce4d9e001e1af5618e1bed040e62961d55
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-21 21:43:29 -0800

    Search Person Hello

--- searchPerson.php
+++ searchPerson.php
@@ -1,11 +1,9 @@
 <?php
-	require_once 'HTTP/Request2.php';
+	//require_once 'HTTP/Request2.php';
 	require_once 'vendor/autoload.php';
 	//require_once 'dbConnect.php';
 
 	use WindowsAzure\Common\ServicesBuilder;
-	use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-	use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 	use MicrosoftAzure\Storage\Common\ServiceException;
 
 	/*function getFaceID($image)
