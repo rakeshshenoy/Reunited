22cb977d65d9d83e8403a7cd25077e9f0bc0a257
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-22 02:23:44 -0800

    Perfect Face ID

--- searchPerson.php
+++ searchPerson.php
@@ -15,7 +15,7 @@ $request->setHeader($headers);
 
 $parameters = array(
     // Request parameters
-    'returnIsIdentical' => 'true'
+    'returnIsIdentical' => 'true',
 );
 
 $url->setQueryVariables($parameters);
@@ -23,7 +23,7 @@ $url->setQueryVariables($parameters);
 $request->setMethod(HTTP_Request2::METHOD_POST);
 
 // Request body
-$request->setBody("{'faceId1':'1bae8ae8-ab68-452d-8c18-cb41a23213e1','faceId2':'5eedf2ab-f981-41c2-8ead-715d8c4b4d2a'}");
+$request->setBody('{"faceId1":"1bae8ae8-ab68-452d-8c18-cb41a23213e1","faceId2":"5eedf2ab-f981-41c2-8ead-715d8c4b4d2a"}');
 
 try
 {
