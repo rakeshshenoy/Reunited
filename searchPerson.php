6e3f88c0c31b2ec5150dc11ab8e2bc892e97be6f
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-29 23:53:27 -0800

    Hello test

--- searchPerson.php
+++ searchPerson.php
@@ -1,3 +1,23 @@
 <?php
+require_once 'HTTP/Request2.php';
+
 echo "Hello!";
+$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/verify');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => 'dd51642516ac431a9c593b4c78b8a806'
+);
+
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+);
+
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_POST);
 ?>
