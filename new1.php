692f99312ec3c5cf8f5e2ebc88ffefea75f71be3
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-30 01:32:33 -0800

    Hello test

--- new1.php
+++ new1.php
@@ -1,25 +1,29 @@
 <?php
-define( 'API_BASE_URL',     'https://westus.api.cognitive.microsoft.com/face/v1.0/verify' );
-//define( 'API_PRIMARY_KEY',      'dd51642516ac431a9c593b4c78b8a806' );
-//$img = 'YOUR IMAGE URL HERE';
+define( 'API_BASE_URL',     'https://westus.api.cognitive.microsoft.com/face/v1.0/detect?' );
+define( 'API_PRIMARY_KEY',      'dd51642516ac431a9c593b4c78b8a806' );
+$img = 'http://media.gq.com/photos/5711559a3c2c86f474dc6d5a/master/pass/chris-paul-3.jpg';
 
-$post_string = '{"faceId1":"fb7fa7f9-7a7b-484c-b7db-0593b3747cfb","faceId2":"706a8182-18c0-4575-93ca-76b2adabe35f"}';
+$post_string = '{"url":"' . $img . '"}';
 
-//$query_params = array(
-//);
+$query_params = array(
+    'analyzesFaceLandmarks'     => 'true',
+    'analyzesAge'                       => 'true',
+    'analyzesGender'                    => 'true',
+    'analyzesHeadPose'              => 'true',
+);
 
-//$params = '';
-//foreach( $query_params as $key => $value ) {
-//    $params .= $key . '=' . $value . '&';
-//}
-//$params .= 'subscription-key=' . API_PRIMARY_KEY;
+$params = '';
+foreach( $query_params as $key => $value ) {
+    $params .= $key . '=' . $value . '&';
+}
+$params .= 'subscription-key=' . API_PRIMARY_KEY;
 
-$post_url = API_BASE_URL;
+$post_url = API_BASE_URL . $params;
 
 $ch = curl_init();
     curl_setopt( $ch, CURLOPT_HTTPHEADER, array(                                                                          
-        'Content-Type: application/json',
-        'Ocp-Apim-Subscription-Key: {dd51642516ac431a9c593b4c78b8a806}',
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($post_string))
     );    
 
     curl_setopt( $ch, CURLOPT_URL, $post_url );
@@ -31,5 +35,4 @@ curl_close( $ch );
 
 print_r( '<pre>' );
 print_r( $response );
-
 ?>
