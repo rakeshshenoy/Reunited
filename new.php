06a32a7448336b2cf3a0cc2298e79164765bbcc1
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-31 02:33:55 -0800

    HTTP Request2 test

--- new.php
+++ new.php
@@ -1,5 +1,5 @@
 <?php
-		require_once 'dbConnect.php'
+		require_once 'dbConnect.php';
 		require_once 'vendor/autoload.php';
 		require_once 'HTTP/Request2.php';
 		use WindowsAzure\Common\ServicesBuilder;
