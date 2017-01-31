76779f053e49b5bfa159b16c1791f52d1558eb94
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-01-31 02:52:01 -0800

    HTTP Request2 test

--- searchPerson.php
+++ searchPerson.php
@@ -2,41 +2,33 @@
 require_once 'vendor/autoload.php';
 require_once 'HTTP/Request2.php';
 
-$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/verify');
-$url = $request->getUrl();
-
-$headers = array(
-    // Request headers
-    'Content-Type' => 'application/json',
-    'Ocp-Apim-Subscription-Key' => 'dd51642516ac431a9c593b4c78b8a806'
-);
-
-$request->setHeader($headers);
-
-$parameters = array(
-    // Request parameters
-);
-
-$url->setQueryVariables($parameters);
-
-$request->setMethod(HTTP_Request2::METHOD_POST);
-
-echo "Hello!";
-
-// Request body
-$request->setBody('{
-    "faceId1":"fb7fa7f9-7a7b-484c-b7db-0593b3747cfb",
-    "faceId2":"706a8182-18c0-4575-93ca-76b2adabe35f"
+$request = new Http_Request2('https://westus.api.cognitive.microsoft.com/face/v1.0/detect');
+				$url = $request->getUrl();
+				$headers = array(
+				    // Request headers
+				    'Content-Type' => 'application/json',
+				    'Ocp-Apim-Subscription-Key' => 'dd51642516ac431a9c593b4c78b8a806',
+				);
+				$request->setHeader($headers);
+				$parameters = array(
+				    // Request parameters
+				    'returnFaceId' => 'true'
+				);
+				$url->setQueryVariables($parameters);
+				$request->setMethod(HTTP_Request2::METHOD_POST);
+				$request->setBody('{
+    "url":"http://media.gq.com/photos/5711559a3c2c86f474dc6d5a/master/pass/chris-paul-3.jpg"
 }');
-
-try
-{
-    $response = $request->send();
-    $x = json_decode($response->getBody(), true);
-    echo($x["confidence"]);
-}
-catch (HttpException $ex)
-{
-    echo $ex;
-}
+				try
+				{
+				    $response = $request->send();
+				}
+				catch (HttpException $ex)
+				{
+				    echo $ex;
+				}
+				$y = $response->getBody();
+				//var_dump($y);
+				$mainFaceID = json_decode($y, true);
+				echo $mainFaceID;
 ?>
